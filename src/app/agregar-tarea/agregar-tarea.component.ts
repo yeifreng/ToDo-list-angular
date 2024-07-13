11ba31d1f5990bfa2d6caa0bc44178d2e8657895
Yeifreng <yeifrenjose@gmail.com>
2024-07-12 22:21:25 -0500

    Creando componente hijo para agregar la tarea por medio de un input

---
+++ src/app/agregar-tarea/agregar-tarea.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-tarea',
+  templateUrl: './agregar-tarea.component.html',
+  styleUrls: ['./agregar-tarea.component.css']
+})
+export class AgregarTareaComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgregarTareaComponent } from './agregar-tarea/agregar-tarea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarTareaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
