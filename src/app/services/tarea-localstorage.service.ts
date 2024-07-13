00f9f3683eb374d25b2821577c3bad9f7378afef
Yeifreng <yeifrenjose@gmail.com>
2024-07-12 22:55:44 -0500

    Creamos el servicio que sera el encargado de agregar la tarea y eliminarla del localstorage asi mismo como de mostrar las tareas sin terminar aun

--- src/app/services/tarea-localstorage.service.ts
+++ src/app/services/tarea-localstorage.service.ts
@@ -5,5 +5,29 @@ import { Injectable } from '@angular/core';
 })
 export class TareaLocalstorageService {
 
-  constructor() { }
+
+  //Creamos una variable que sera la key de nuestro localstorage
+
+  private localStorageKey = 'tareas';
+
+  //Realizamos un get para que nos traiga las tareas del localstorage que tenga la key localStorageKey
+  getTareas():string[] {
+
+    return JSON.parse(localStorage.getItem(this.localStorageKey) as string) || [];
+
+  }
+
+  //Creamos una función que nos permita agregar la tarea
+
+  agregarTarea(tarea: string){
+    const tareas = this.getTareas(); // guardamos la tarea en la variable
+    tareas.push(tarea); // hacemos un push para que se añana al array
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tareas)); // mostramos las tareas que esten el el localstorage con el key localStorageKey
+  }
+
+  eliminarTarea(index: number){
+    const tareas = this.getTareas(); //guardamos la tarea en la variable
+    tareas.splice(index,1);  // eliminamos la tarea de acuerdo al indice
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tareas)); // mostramos las tareas que esten el el localstorage con el key localStorageKey
+  }
 }
