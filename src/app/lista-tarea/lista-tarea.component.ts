8625813883031f763bc843ba0cf94d0e26ab8cc2
Yeifreng <yeifrenjose@gmail.com>
2024-07-12 23:04:39 -0500

    Creamos un array para listar las tareas ingresadas, inyectamos el servicio, creamos los metodos agregar y eliminar tarea y mostramos la lista de tareas desde el componente padre lista-tarea

--- src/app/lista-tarea/lista-tarea.component.ts
+++ src/app/lista-tarea/lista-tarea.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TareaLocalstorageService } from '../services/tarea-localstorage.service';
 
 @Component({
   selector: 'app-lista-tarea',
@@ -7,12 +8,29 @@ import { Component } from '@angular/core';
 })
 export class ListaTareaComponent {
 
+  listaTareas:string[] = []; //creamos un array para mostrar todas las tareas
+
+
   //5. Creamos la variable que se usara para recibir el valor que envia el componente agregar-tarea por medio del input
   tareaRecibida: string = '';
+
+  constructor(private _tareaService:TareaLocalstorageService){
+    this.listaTareas = this._tareaService.getTareas(); //Asignamos la lista de tareas que se encuentran en el momento a la variable
+  }
 
   //6. Creamos el metodo que recibira el evento desde el componente agregar-lista y le asignamos el valor a la variable
   agregarTarea($event:string){
     this.tareaRecibida = $event
+    if(this.tareaRecibida != ''){
+      this._tareaService.agregarTarea(this.tareaRecibida); // agregamos la tarea
+      this.tareaRecibida = ''; //limpiamos el input
+      this.listaTareas = this._tareaService.getTareas(); // traemos la lista para que nos muestre la nueva tarea
+      }
+  }
+
+  eliminarTarea(index: number){
+    this._tareaService.eliminarTarea(index);
+    this.listaTareas = this._tareaService.getTareas(); // traemos la lista para que nos muestre sin la tarea eliminada
   }
 
 
