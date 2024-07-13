070361d0524f832e7c1b65fc8e1007dc8a686b27
Yeifreng <yeifrenjose@gmail.com>
2024-07-12 22:53:47 -0500

    Enviamos desde el componente hijo agregar-tarea el valor de la tarea por medio del decorador output hacia el componente padre lista-tarea para mostrarlo en un listado

--- src/app/agregar-tarea/agregar-tarea.component.ts
+++ src/app/agregar-tarea/agregar-tarea.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-agregar-tarea',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregar-tarea.component.css']
 })
 export class AgregarTareaComponent {
+
+  //1. Creamos la variable que recibira el valor que escribamos en el input
+  tarea:string = '';
+
+  //2. la variable output se encargara de realizar la comunicacion con al componente lista-tarea para hacerle llegar el mensaje
+  @Output() tareaEnviar = new EventEmitter<string>();
+
+  //3. Creamos un metodo que se encargara de emitir la tarea que se escirbe en le input
+  enviarTarea(){
+    this.tareaEnviar.emit(this.tarea)
+  }
+
 
 }
--- src/app/lista-tarea/lista-tarea.component.ts
+++ src/app/lista-tarea/lista-tarea.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ListaTareaComponent {
 
+  //5. Creamos la variable que se usara para recibir el valor que envia el componente agregar-tarea por medio del input
+  tareaRecibida: string = '';
+
+  //6. Creamos el metodo que recibira el evento desde el componente agregar-lista y le asignamos el valor a la variable
+  agregarTarea($event:string){
+    this.tareaRecibida = $event
+  }
+
+
+
+
 }
